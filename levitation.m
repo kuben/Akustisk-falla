b64e53d1f541a8e822803139fc1be179d54e10c7
kuben <jakub.fojt@wp.pl>
2018-01-26 15:15:09 +0100

    Fixat axlar på figurer, skapat generera_p

--- levitation.m
+++ levitation.m
@@ -1,38 +1,26 @@
 clear, latex
 
-omega = 2*pi*40e3;
+% omega = 2*pi*40e3;
+
 c_0 = 346.13;
+omega = 2*pi*c_0/0.2*23.25;
 k = omega/c_0; %Vågtal
 r = 10e-3;
 P_0 = 1e3;
 
+generera_p;
 
-% syms x y z 
-
-% d = sqrt(x^2 + y^2 + z^2);
-% sintheta = sqrt(x^2 + y^2)/d;
-
-% p = P_0*besselj(0,k*r*sintheta)/d*exp(1i*k*d);%Givet att transducern är i origo
-% px = diff(p,x);
-% py = diff(p,y);
-% pz = diff(p,z);
-
-p = @(x,y,z)(1000.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(1./2);
-px = @(x,y,z)(x.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))).*798363078001360625i)./(1099511627776.*(x.^2 + y.^2 + z.^2)) - (1000.*x.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(3./2) - (1000.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*((80000.*pi.*x)./(34613.*(x.^2 + y.^2).^(1./2).*(x.^2 + y.^2 + z.^2).^(1./2)) - (80000.*pi.*x.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(3./2))).*besselj(1, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(1./2);
-py = @(x,y,z)(y.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))).*798363078001360625i)./(1099511627776.*(x.^2 + y.^2 + z.^2)) - (1000.*y.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(3./2) - (1000.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*((80000.*pi.*y)./(34613.*(x.^2 + y.^2).^(1./2).*(x.^2 + y.^2 + z.^2).^(1./2)) - (80000.*pi.*y.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(3./2))).*besselj(1, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(1./2);
-pz = @(x,y,z)(z.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))).*798363078001360625i)./(1099511627776.*(x.^2 + y.^2 + z.^2)) - (1000.*z.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*besselj(0, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(x.^2 + y.^2 + z.^2).^(3./2) + (80000000.*z.*pi.*exp(((x.^2 + y.^2 + z.^2).^(1./2).*6386904624010885i)./8796093022208).*(x.^2 + y.^2).^(1./2).*besselj(1, (80000.*pi.*(x.^2 + y.^2).^(1./2))./(34613.*(x.^2 + y.^2 + z.^2).^(1./2))))./(34613.*(x.^2 + y.^2 + z.^2).^2);
-
-T_pos = [0.1 0 0;
-         -0.1 0 0];
+T_pos = [0.1 0 0.05;
+         -0.1 0 0.05];
 T_pitch =[pi; %vinkel mellan proj(normalen) och x-axeln, 0 är i x-led
          0];
-T_yaw = [pi/4; %vinkel mellan radiellt led och z-axeln, 0 är i radiellt led
-         pi/4];
+T_yaw = [0; %vinkel mellan radiellt led och z-axeln, 0 är i radiellt led
+         0];
 
 
-x_vekt = linspace(-0.1,0.1,1000);
-y_vekt = linspace(-0.1,0.1,1000);
-z_vekt = linspace(0,0.1,1000);
+x_vekt = linspace(-0.1,0.1,500);
+y_vekt = linspace(-0.1,0.1,500);
+z_vekt = linspace(0,0.1,500);
 
 %Plotta i planet y = 0
 [x,z] = meshgrid(x_vekt, z_vekt);
@@ -66,48 +54,63 @@ for i = 1:size(T_pos,1)
     pz_sum = pz_sum + pz(x_rot, y_rot, z_rot);
     stryk = stryk | ((x_rot.^2 + y_rot.^2 + z_rot.^2) < r_far.^2);
 end
-x(stryk) = NaN;
-z(stryk) = NaN;
-p_sum(stryk) = NaN;
-px_sum(stryk) = NaN;
-py_sum(stryk) = NaN;
-pz_sum(stryk) = NaN;
+% x(stryk) = NaN;
+% z(stryk) = NaN;
+% p_sum(stryk) = NaN;
+% px_sum(stryk) = NaN;
+% py_sum(stryk) = NaN;
+% pz_sum(stryk) = NaN;
 
+isolines = logspace(0,2.4,50);
+isolines = exp(isolines);
 figure(1)
-contourf(x,z,abs(p_sum))
+contourf(x,z,abs(p_sum),isolines)
 title('Tryck $p$')
 xlabel('x [m]')
 ylabel('z [m]')
 colorbar
 axis tight
+caxis([4 9e4])
 
+isolines = logspace(-8,-5,50);
+% isolines = exp(isolines);
 figure(2)
 gor = gorkov(p_sum, px_sum, py_sum, pz_sum);
-contourf(x,z,gor)
+contourf(x,z,gor,isolines)
 title('Gorkovpotential [Nm]')
 xlabel('x [m]')
 ylabel('z [m]')
 colorbar
 axis tight
+caxis([0 1e-5])
 
 figure(3),clf,hold on
-[u,v] = gradient(gor);
+[u,v] = gradient(gor,x_vekt,z_vekt);
 u = -u; v = -v;
-skip = 5;
-quiver(x(1:skip:end,1:skip:end),z(1:skip:end,1:skip:end),...
-    u(1:skip:end,1:skip:end),v(1:skip:end,1:skip:end))
-title('$\nabla$ Gorkovpotential [N]')
+skip = 20;
+u_plot = u(1:skip:end,1:skip:end);
+max = 2e-3;
+u_plot(abs(u_plot) > max) = sign(u_plot(abs(u_plot) > max))*max;
+v_plot = v(1:skip:end,1:skip:end);
+v_plot(abs(v_plot) > max) = sign(v_plot(abs(v_plot) > max))*max;
+x_plot = x(1:skip:end,1:skip:end);
+z_plot = z(1:skip:end,1:skip:end);
+quiver(x_plot,z_plot,u_plot,v_plot)
+title('$-\nabla$ Gorkovpotential [N]')
 xlabel('x [m]')
 ylabel('z [m]')
-xlim([-2e-2,2e-2])
-ylim([0,4e-2])
-% axis equal
+% xlim([-2e-2,2e-2])
+% ylim([0,4e-2])
+axis equal
+axis tight
 
+isolines = logspace(-10,-3,50);
 figure(4)
-lapl = divergence(u,v,x,z)
-contourf(x,z,gor)
-title('$\nabla^2$ Gorkovpotential')
+lapl = divergence(u,v,x,z);
+contourf(x,z,gor,isolines)
+title('$-\nabla^2$ Gorkovpotential')
 xlabel('x [m]')
 ylabel('z [m]')
 colorbar
 axis tight
+caxis([1e-10 1e-5])
