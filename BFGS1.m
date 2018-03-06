2ec9a42d9e436307c48e49d8a12591cf11056e08
LazerBuouy <35767167+LazerBuouy@users.noreply.github.com>
2018-03-06 11:11:07 +0100

    Update BFGS1.m
    Latest probably final version of fixed phases maximum of laplacian of Gorkov potential finder.

--- BFGS1.m
+++ BFGS1.m
@@ -1,7 +1,9 @@
-function [minLapl] = BFGS1(startPosition)
+function [minLapl,minLaplVal] = BFGS1(startPosition)
 % Calculates a minimum of the laplacian of the Gor'kov
 % given a start position.
-% Needs lapl.m.
-
-    minLapl = fminunc(@lapl,startPosition);
+% Needs laplFun.m.
+    options = optimoptions('fminunc', 'SpecifyObjectiveGradient', true, ... 'HessUpdate','bfgs'...
+    'OptimalityTolerance',1e-6,'MaxFunctionEvaluations',100*3,...
+    'StepTolerance',1e-8);
+    [minLapl,minLaplVal] = fminunc(@laplFun,startPosition,options);
 end
