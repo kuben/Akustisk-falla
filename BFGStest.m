bfac94af2bef80ccc508124df308228f2edeb0f0
LazerBuouy <35767167+LazerBuouy@users.noreply.github.com>
2018-03-06 11:09:10 +0100

    Update BFGStest.m
    Changed from specified HessUpdate to supplying gradient.

--- BFGStest.m
+++ BFGStest.m
@@ -9,7 +9,7 @@ diam = N*c_0/f;
 % diam = 4e-2;
 
 Transducer.clear_all()
-Transducer.add_circle([0 0 0],4,diam,[0 0 1],0,0)
+Transducer.add_circle([0 0 0],3,diam,[0 0 1],0,0)
 % [tot_p,tot_px,tot_py,tot_pz,near] = Transducer.total_tryck(pos,r_far);
 
 
@@ -18,8 +18,8 @@ X = [0,0,0];
 % X = [-15e-3,15e-3,15e-3];
 
 %[p_sum,px_sum,py_sum,pz_sum,~] = Transducer.total_tryck([X(:) X(:) X(:)])
-options = optimoptions('fminunc','HessUpdate','bfgs'...
-    ,'OptimalityTolerance',1e-6,'MaxFunctionEvaluations',100*3,...
+options = optimoptions('fminunc', 'SpecifyObjectiveGradient', true, ... 'HessUpdate','bfgs'...
+    'OptimalityTolerance',1e-6,'MaxFunctionEvaluations',100*3,...
     'StepTolerance',1e-8);
 lapl = laplFun(X);
 minLaplPos = fminunc(@laplFun,X,options);
@@ -33,7 +33,7 @@ disp(minLapl)
 Transducer.draw_all(4);
 % %Transducer.draw_plane_at_y(0,[],[],[],4)
 z_plane = [sign(minLaplPos(3))*0.07, 100*abs(minLaplPos(3))];
-z_plane = max(z_plane(find(abs(z_plane) <= 0.07)));
+z_plane = max(z_plane(logical(abs(z_plane) <= 0.07)));
 Transducer.draw_plane_at_z(z_plane,[],[],[],4);
 % %Transducer.draw_plane_at_z(2e-2,[],[],[],4)
 hold on; plot3(100*minLaplPos(1),100*minLaplPos(2),100*minLaplPos(3),'x','Color','r')
