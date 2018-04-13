1f2980f36afe232c75a340ee75499e56fc71500a
LazerBuouy <35767167+LazerBuouy@users.noreply.github.com>
2018-04-13 14:50:14 +0200

    Update Exempel.m
    Lade till produkten som ett exempel. Höjden kommer behöva justeras senare.

--- Exempel.m
+++ Exempel.m
@@ -1,25 +1,25 @@
-%% Låt figurer ha LaTeX fonts
+%% LÃ¥t figurer ha LaTeX fonts
 latex_fonts
 
-%% Lägg till en transducer
-%Argumenten är ([x y z], pitch, yaw)
+%% LÃ¤gg till en transducer
+%Argumenten Ã¤r ([x y z], pitch, yaw)
 %pitch och yaw anger transducerns riktning enligt
-%   0 < pitch < 2*pi är rotation sett ovanifrån (dvs z-axeln). 0 är i x-led.
-%   -pi/2 < yaw < pi/2 är rotation runt sidoaxel. 0 är i radiellt led,
-%                       pi/2 uppåt, -pi/2 nedåt.
+%   0 < pitch < 2*pi Ã¤r rotation sett ovanifrÃ¥n (dvs z-axeln). 0 Ã¤r i x-led.
+%   -pi/2 < yaw < pi/2 Ã¤r rotation runt sidoaxel. 0 Ã¤r i radiellt led,
+%                       pi/2 uppÃ¥t, -pi/2 nedÃ¥t.
 
-%Kommandot skapar ett Transducer-objekt och lägger det i en lista
+%Kommandot skapar ett Transducer-objekt och lÃ¤gger det i en lista
 %Skapa transducer som pekar rakt i x-led
 Transducer.add_single(1e-2*[-2 0 0],0,0)
 
-%Skapa transducer som pekar snett uppåt i y-led
+%Skapa transducer som pekar snett uppÃ¥t i y-led
 Transducer.add_single(1e-2*[2 0 0],pi,pi/4)
 
-%Skapa transducer som pekar rakt nedåt (pitch spelar ingen roll här)
+%Skapa transducer som pekar rakt nedÃ¥t (pitch spelar ingen roll hÃ¤r)
 Transducer.add_single(1e-2*[0 0 5],0,-pi/2)
 
 %% Rita ut alla transducers i listan
-%För tillfället ritas området
+%FÃ¶r tillfÃ¤llet ritas omrÃ¥det
 % -5cm < x < 5cm
 % -5cm < y < 5cm
 % -5cm < z < 10cm ut
@@ -32,21 +32,21 @@ Transducer.draw_all(1)
 Transducer.draw_plane_at_x(0.03,1,[],[],[])
 Transducer.draw_plane_at_y(0.03,1,[],[],[])
 Transducer.draw_plane_at_z(0.03,1,[],[],[])
-%% OBS Transducers försvinner inte ur listan av sig själva, för att ta bort dem
+%% OBS Transducers fÃ¶rsvinner inte ur listan av sig sjÃ¤lva, fÃ¶r att ta bort dem
 Transducer.clear_all()
 
-%% Listan är en global variabel, för att se den
+%% Listan Ã¤r en global variabel, fÃ¶r att se den
 global transducer_list
 transducer_list
 
-%% För att smidigt lägga till transducers i en cirkel
-% Använd add_circle(pos,N,radius,normal)
-% pos är mittpunkt
-% N är antal transducers
-% radius är cirkelns radie
-% Normal är en vektor som pekar ut från cirkeln (behöver ej vara normerad)
+%% FÃ¶r att smidigt lÃ¤gga till transducers i en cirkel
+% AnvÃ¤nd add_circle(pos,N,radius,normal)
+% pos Ã¤r mittpunkt
+% N Ã¤r antal transducers
+% radius Ã¤r cirkelns radie
+% Normal Ã¤r en vektor som pekar ut frÃ¥n cirkeln (behÃ¶ver ej vara normerad)
 
-%% Cirkel med 6 transducers i xy-planet, normalen är z-axeln
+%% Cirkel med 6 transducers i xy-planet, normalen Ã¤r z-axeln
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1])
 Transducer.draw_all()
 
@@ -62,19 +62,36 @@ Transducer.clear_all()
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1],pi/6)
 Transducer.draw_all()
 
-%% Vridning 180 grader (för att peka utåt)
+%% Vridning 180 grader (fÃ¶r att peka utÃ¥t)
 Transducer.clear_all()
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1],pi)
 Transducer.draw_all()
 
-%% rel_yaw roterar transducers "framåt" och "bakåt"
-%% Transducers riktade snett uppåt
+%% rel_yaw roterar transducers "framÃ¥t" och "bakÃ¥t"
+%% Transducers riktade snett uppÃ¥t
 Transducer.clear_all()
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1],0,pi/4)
 Transducer.draw_all()
 
-%% phi_0 förskjuter transducers lite, se skillnaden mellan följande två kommandon
+%% phi_0 fÃ¶rskjuter transducers lite, se skillnaden mellan fÃ¶ljande tvÃ¥ kommandon
 Transducer.clear_all()
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1],0,0,0)
 Transducer.add_circle([0 0 0],6,4e-2,[0 0 1],0,0,pi/24)
+Transducer.draw_all()
+
+%% Produkten
+Transducer.clear_all()
+% Vital constants for the geometry
+Rbase = 12e-3; % m
+height = 12e-2; % m
+% The lower plate of transducers
+Transducer.add_single([0,0,-height/2],0,pi/2);
+for i = 1:4
+    Transducer.add_circle([0,0,-height/2],6*i,i*Rbase,[0,0,1],0,pi/2); % 6*
+end
+%The upper plate of transducers
+Transducer.add_single([0,0,height/2],0,-pi/2);
+for i = 1:4
+    Transducer.add_circle([0,0,height/2],6*i,i*Rbase,[0,0,1],0,-pi/2); % 6*
+end
 Transducer.draw_all()
